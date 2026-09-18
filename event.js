e7ed56e16b788b0e273a3e2d44b9b7944aad3511
yashtyagi1222-png <yashtyagi1222@gmail.com>
2026-09-18 10:31:32 +0530

    Add files via upload

--- event.js
+++ event.js
@@ -1,2 +1,20 @@
-import EventEmitter from 'node:events';
-const emitter = new EventEmitter();
+import EventEmitter from "node:events";
+
+const emitter = new EventEmitter()
+
+//Register Event
+emitter.on("Orderplaced", (orderName,value)=>{
+    console.log(`Order placed for ${orderName} with value ${value} has been placed successfully`);
+
+})
+emitter.on("Orderplaced", ()=>{
+    console.log('Notification has been send successfully');
+})
+
+emitter.on("Orderplaced", ()=>{
+    console.log('inventory has been updated successfully');
+});
+
+//trigger the event
+emitter.emit("Orderplaced", "Iphone 18 pro max", 100000)
+emitter.listenerCount("Oder placed")
